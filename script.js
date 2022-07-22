1a3f6925084a162aeeac147cec7bdcc6b05c2fce
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-22 07:56:17 -0400

    added in moment.js and local storage

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var currentHour = moment().hour();
 // moment js functionality
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-
+// once save button is clicked the data is saved
+$(".saveButton").on("click", function () {
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, text);
+});
 
   
